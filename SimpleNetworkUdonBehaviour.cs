9c268e192ee56424bcbe41e7fdda5b7cdafe7523
tutinoco <m@tutino.co>
2023-02-07 23:25:49 +0900

    自身のPCのみでイベントを実行するExecEventメソッドを新たに追加

--- SimpleNetworkUdonBehaviour.cs
+++ SimpleNetworkUdonBehaviour.cs
@@ -35,7 +35,7 @@ namespace tutinoco
                 if( _ignoreJoinSync && Networking.LocalPlayer.isMaster ) _isIgnoredJoinSync = true;
                 if( Networking.IsOwner(gameObject) || !_isStandby ) return;
                 if( _ignoreJoinSync && !_isIgnoredJoinSync ) { _isIgnoredJoinSync=true; return; }
-                ExecuteEvents();
+                _Receives();
             }
         }
 
@@ -60,21 +60,21 @@ namespace tutinoco
         {
             if( !IsPublisher() && !force ) return;
 
-            string c = (_isStandby?GenerateCode():cmds+"･")+name+":"+value;
+            string c = (_isStandby?_GenerateCode():cmds+"･")+name+":"+value;
             _isStandby = false;
             cmds = c;
 
             if( !Networking.IsOwner(gameObject) ) Networking.SetOwner(Networking.LocalPlayer, gameObject);
             else {
-                if( _clientSimMode ) SendCustomEvent("_RequestExecuteEvnets");
+                if( _clientSimMode ) SendCustomEvent("_RequestReceives");
                 else RequestSerialization();
             }
         }
 
-        public void _RequestExecuteEvnets()
+        public void _RequestReceives()
         {
             if( _isStandby ) return;
-            ExecuteEvents();
+            _Receives();
             _isStandby = true;
         }
 
@@ -84,13 +84,15 @@ namespace tutinoco
             RequestSerialization();
         }
 
-        public override void OnPreSerialization() { _RequestExecuteEvnets(); }
+        public override void OnPreSerialization() { _RequestReceives(); }
+
+        public void ExecEvent(string name, string value="") { ReceiveEvent(name, value); }
 
         public virtual void ReceiveEvent(string name, string value) { }
 
-        private string GenerateCode() { return ""+((int)UnityEngine.Random.Range(10000000,100000000)); }
+        private string _GenerateCode() { return ""+((int)UnityEngine.Random.Range(10000000,100000000)); }
 
-        private void ExecuteEvents()
+        private void _Receives()
         {
             if( !_isInitialized ) {
                 Debug.LogWarning("Reception was rejected because SimpleNetworkUdonBehaviour has not been initialized. The received data is as follows: "+cmds);
@@ -109,21 +111,25 @@ namespace tutinoco
         
         // bool
         public void SendEvent(string name, bool value, bool force=false) { SendEvent(name, _Bool(value), force); }
+        public void ExecEvent(string name, bool value) { ExecEvent(name, _Bool(value)); }
         public static bool GetBool( string v ) { return v!="0"; }
         private static string _Bool( bool v ) { return v?"1":"0"; }
 
         // int
         public void SendEvent(string name, int value, bool force=false) { SendEvent(name, _Int(value), force); }
+        public void ExecEvent(string name, int value) { ExecEvent(name, _Int(value)); }
         public static int GetInt( string v ) { return int.Parse(v); }
         private static string _Int( int v ) { return ""+v; }
 
         // float
         public void SendEvent(string name, float value, bool force=false) { SendEvent(name, _Float(value), force); }
+        public void ExecEvent(string name, float value) { ExecEvent(name, _Float(value)); }
         public static float GetFloat( string v ) { return float.Parse(v); }
         private static string _Float( float v ) { return ""+v; }
 
         // Vector3
         public void SendEvent(string name, Vector3 value, bool force=false) { SendEvent(name, _Vector3(value), force); }
+        public void ExecEvent(string name, Vector3 value) { ExecEvent(name, _Vector3(value)); }
         public static Vector3 GetVector3( string v ) { string[] d=v.Split(','); return new Vector3(float.Parse(d[0]), float.Parse(d[1]), float.Parse(d[2])); }
         private static string _Vector3( Vector3 v ) { return v.x+","+v.y+","+v.z; }
     }
